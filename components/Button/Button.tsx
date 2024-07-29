2aa24315d1a99d9af5474fbb9296521d8bbdfe09
mllamazares <mmllamazares@gmail.com>
2024-07-28 17:39:01 -0700

    Add React Icons

--- components/Button/Button.tsx
+++ components/Button/Button.tsx
@@ -1,9 +1,10 @@
 import React from 'react'
+import { MdMenu } from "react-icons/md";
 
 const Button = () => {
   return (
     <>
-        <button className="absolute z-10 top-0 right-0 w-16 h-8 bg-blanco border-primary-blue border-2 shadow-md text-primary-blue text-center m-4">Menu</button>
+        <button className="absolute z-10 top-0 right-0 w-10 h-10 bg-blanco border-primary-blue border-2 shadow-md text-primary-blue  m-4 flex items-center justify-center"><MdMenu/></button>
     </>
   )
 }
--- components/NotificationPanel/NotificationPanel.tsx
+++ components/NotificationPanel/NotificationPanel.tsx
@@ -1,17 +1,18 @@
 import React from 'react'
 import Notification from '../Notification/Notification'
+import { MdNotifications, MdClose } from "react-icons/md";
 
 const NotificationPanel = () => {
   return (
     <div className='absolute right-0 h-screen w-96 bg-secondary-blue flex flex-col z-20'>
         <div className='relative h-screen'>
-            <h2 className='text-blanco text-2xl font-Roboto-Bold text-center m-6'>Notificaciones</h2>
+            <h2 className='text-blanco text-2xl font-Roboto-Bold text-center m-6 flex items-center justify-center'><MdNotifications/> Notificaciones</h2>
             <Notification/>
             <Notification/>
             <Notification/>
             <Notification/>
             <div className='absolute w-[92%] bottom-0 right-0 flex justify-between m-4'>
-                <button className='w-12 h-12 text-blanco font-Roboto-Bold'>X</button>
+                <button className='w-12 h-12 text-blanco font-Roboto-Bold'><MdClose size={"2em"}/></button>
                 <button className='h-12 w-32 text-blanco font-Roboto-Bold'>Limpiar Todo</button>
             </div>
         </div>
