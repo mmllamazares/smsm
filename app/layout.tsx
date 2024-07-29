59efb1d2d7f8aee7b44eec03d88c0874ca4736c3
mllamazares <mmllamazares@gmail.com>
2024-07-29 00:38:06 -0700

    reseteando posision de botones de zoom

--- app/layout.tsx
+++ app/layout.tsx
@@ -19,8 +19,8 @@ export default function RootLayout({
   return (
     <html lang="es">
       <body className={inter.className}>
-        <NavBar />
-        <NotificationButton />
+        {/* <NavBar />
+        <NotificationButton /> */}
         {children}
       </body>
     </html>
