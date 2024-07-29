b5ae1a517f1e8e6980ea3280336af639f0c4359a
mllamazares <mmllamazares@gmail.com>
2024-07-28 23:57:56 -0700

    Add funcionalidad para abrir y cerrar el menu

--- components/NotificationPanel/NotificationPanel.tsx
+++ components/NotificationPanel/NotificationPanel.tsx
@@ -1,10 +1,12 @@
+'use client'
 import React from 'react'
 import Notification from '../Notification/Notification'
 import { MdNotifications, MdClose } from "react-icons/md";
 
-const NotificationPanel = () => {
+const NotificationPanel = ({isActive, handleClick}:any) => {
+ 
   return (
-    <div className='absolute right-0 h-screen w-96 bg-secondary-blue flex flex-col z-20'>
+    <div className={`absolute ${isActive ? "invisible" : ""} right-0 h-screen w-96 bg-secondary-blue flex flex-col z-20`}>
         <div className='relative h-screen'>
             <h2 className='text-blanco text-2xl font-Roboto-Bold text-center m-6 flex items-center justify-center'><MdNotifications/> Notificaciones</h2>
             <Notification/>
@@ -12,7 +14,7 @@ const NotificationPanel = () => {
             <Notification/>
             <Notification/>
             <div className='absolute w-[92%] bottom-0 right-0 flex justify-between m-4'>
-                <button className='w-12 h-12 text-blanco font-Roboto-Bold'><MdClose size={"2em"}/></button>
+                <button className='w-12 h-12 text-blanco font-Roboto-Bold' onClick={handleClick}><MdClose size={"2em"}/></button>
                 <button className='h-12 w-32 text-blanco font-Roboto-Bold'>Limpiar Todo</button>
             </div>
         </div>
