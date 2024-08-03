7d01d88a1e86d3f6bb553f3c4bc77ba32a69f1b5
mllamazares <mmllamazares@gmail.com>
2024-08-03 01:48:43 -0400

    Add prioridad true a la imagen de logo para que siempre se cargue

--- app/login/page.tsx
+++ app/login/page.tsx
@@ -5,7 +5,7 @@ const Login = () => {
   return (
     <div className="flex justify-center items-center w-screen h-screen">
       <div className="flex flex-col justify-center items-center gap-3 p-10 w-80 font-Roboto-Regular">
-        <Image src={Logo} alt="Logo" width={250} height={250} />
+        <Image src={Logo} alt="Logo" width={250} height={250} priority={true} />
         {/* <div className='flex flex-col items-start gap-3'>
                 <label htmlFor="user">Usuario:</label>
                 <input className='border-2 border-secondary-light-blue p-2 accent-primary-blue' id='user' type="text" />
