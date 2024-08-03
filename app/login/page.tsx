eb82cd8d66f8750ae4bac0013d7ecf0473b60cd4
mllamazares <mmllamazares@gmail.com>
2024-08-03 01:31:14 -0400

    Instalado y configurado Flowbite ademas de mejorar el login

--- app/login/page.tsx
+++ app/login/page.tsx
@@ -6,15 +6,37 @@ const Login = () => {
     <div className='flex justify-center items-center w-screen h-screen'>
         <div className='flex flex-col justify-center items-center gap-3 p-10 w-80 font-Roboto-Regular'>
             <Image src={Logo} alt='Logo' width={250} height={250}/>
-            <div className='flex flex-col items-start gap-3'>
+            {/* <div className='flex flex-col items-start gap-3'>
                 <label htmlFor="user">Usuario:</label>
                 <input className='border-2 border-secondary-light-blue p-2 accent-primary-blue' id='user' type="text" />
             </div>
             <div className='flex flex-col items-start gap-3'>
                 <label htmlFor="password">Contraseña:</label>
                 <input className='border-2 border-secondary-light-blue p-2 accent-primary-blue' id='password' type="password" />
-            </div>
+            </div> */}
+
+<form className="max-w-sm mx-auto font-Roboto-Regular">
+  <div className="mb-5">
+    <label htmlFor="text" className="block mb-2 text-sm font-medium text-gray-900 dark:text-white">Usuario:</label>
+    <input type="email" id="email" className="bg-gray-50 border border-secondary-light-blue text-gray-900 text-sm  focus:ring-primary-blue focus:border-primary-blue block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-primary-blue dark:focus:border-primary-blue" required />
+  </div>
+  <div className="mb-5">
+    <label htmlFor="password" className="block mb-2 text-sm font-medium text-gray-900 dark:text-white">Contraseña:</label>
+    <input type="password" id="password" className="bg-gray-50 border border-secondary-light-blue text-gray-900 text-sm  focus:ring-primary-blue focus:border-primary-blue block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-primary-blue dark:focus:border-primary-blue" required />
+  </div>
+  <div className="flex items-start mb-5">
+    <div className="flex items-center h-5">
+      <input id="remember" type="checkbox" value="" className="w-4 h-4 border border-secondary-light-blue bg-gray-50 focus:ring-3 focus:ring-primary-blue checked:bg-primary-blue dark:bg-gray-700 dark:border-gris dark:focus:ring-primary-blue dark:ring-offset-gray-800 dark:focus:ring-offset-gray-800" required />
+    </div>
+    <label htmlFor="remember" className="ms-2 text-sm font-medium text-gray-900 dark:text-gray-300">Remember me</label>
+  </div>
+  <button type="submit" className="text-white bg-primary-blue hover:bg-secondary-blue focus:ring-4 focus:outline-none focus:ring-blue-300 font-medium  text-sm w-full sm:w-auto px-5 py-2.5 text-center dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800">Inicio de sesión</button>
+</form>
         </div>
+
+
+
+
     </div>
   )
 }
--- tailwind.config.ts
+++ tailwind.config.ts
@@ -2,16 +2,44 @@ import type { Config } from "tailwindcss";
 
 const config: Config = {
   content: [
+    "./node_modules/flowbite-react/lib/**/*.js",
     "./pages/**/*.{js,ts,jsx,tsx,mdx}",
     "./components/**/*.{js,ts,jsx,tsx,mdx}",
     "./app/**/*.{js,ts,jsx,tsx,mdx}",
+    "./public/**/*.html",
+    "./layouts/**/*.html", "./content/**/*.md", "./content/**/*.html", "./src/**/*.js",
   ],
+  // make sure to safelist these classes when using purge
+  safelist: [
+    'w-64',
+    'w-1/2',
+    'rounded-l-lg',
+    'rounded-r-lg',
+    'bg-gray-200',
+    'grid-cols-4',
+    'grid-cols-7',
+    'h-6',
+    'leading-6',
+    'h-9',
+    'leading-9',
+    'shadow-lg'
+  ],
+
+  // enable dark mode via class strategy
+  darkMode: 'class',
   theme: {
     extend: {
       backgroundImage: {
         "gradient-radial": "radial-gradient(var(--tw-gradient-stops))",
         "gradient-conic":
           "conic-gradient(from 180deg at 50% 50%, var(--tw-gradient-stops))",
+      },
+      spacing: {
+        '128': '32rem',
+        '144': '36rem',
+      },
+      borderRadius: {
+        '4xl': '2rem',
       },
       colors: {
         "primary-blue": "#11467c",
@@ -27,10 +55,14 @@ const config: Config = {
       },
       fontFamily:{
         "Roboto-Bold": ['Roboto-Bold'],
-        "Roboto-Regular": ['Roboto-Regular']
+        "Roboto-Regular": ['Roboto-Regular'],
+        sans: ['Graphik', 'sans-serif'],
+        serif: ['Merriweather', 'serif'],
       }
     },
   },
-  plugins: [],
+  plugins: [
+    require("flowbite/plugin")
+  ],
 };
 export default config;
